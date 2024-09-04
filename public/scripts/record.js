bfa14e69096759befd896645998963c83801a1ae
Tin04 <oscargu5763@gmail.com>
2024-09-04 18:03:14 +0800

    feat: module for sending item records to server

---
+++ public/scripts/record.js
@@ -0,0 +1,29 @@
+const Record = (function() {
+    // This function sends a record request to the server
+
+    const record = function(itemName, itemTag, itemQuantity, itemPrice, action, onSuccess, onError) {
+        const data = JSON.stringify({itemName, itemTag, itemQuantity, itemPrice, action});
+
+        fetch("/record", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: data
+        })
+        .then((res) => res.json() )
+        .then((json) => {
+            if (json.status === "error") {
+                if (onError) onError(json.error);
+            }
+            if (json.status === "success") {
+                if (onSuccess) onSuccess();
+            }
+        })
+        .catch((err) => {
+            console.log("Error!");
+        });
+    };
+    
+    return { record };
+})();
