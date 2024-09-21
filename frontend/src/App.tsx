dc4277bab68db1a44719742002aebdcfd8ce8270
Tin04 <oscargu5763@gmail.com>
2024-09-21 22:19:54 +0800

    feat: udpate App structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './pages/About';
 import History from './pages/History';
 import Links from './pages/Links';
 
@@ -10,13 +9,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/links" element={<Links />} />
-        <Route path="/about" element={<About />} />
-        
-      </Routes>
+      <div id="app-body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/links" element={<Links />} />
+          
+        </Routes>
+      </div>
     </div>
   );
 };
