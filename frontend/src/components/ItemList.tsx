d3f79163bca9c37d72fbc182d7d83138444d5647
Tin04 <oscargu5763@gmail.com>
2024-09-21 22:37:07 +0800

    feat: item list in React

---
+++ frontend/src/components/ItemList.tsx
@@ -0,0 +1,57 @@
+import React, { useMemo } from 'react';
+import '/styles/style.css';
+
+interface Item {
+  itemTag: string;
+  quantity: number;
+  avgCost: number;
+  profit: number;
+  price: number;
+}
+
+const ItemList = React.memo(({ items }: { items: Record<string, Item>[] }) => {
+  const itemEntries = Object.entries(items);
+
+  if (itemEntries.length === 0) {
+    return <div>No items to display.</div>;
+  }
+  return (
+      <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Average Cost</th>
+            <th>Quantity</th>
+        </tr>
+        </thead>
+        <tbody>
+          {itemEntries.map(([key, item]) => (
+                <tr key={key}
+                  className={
+                    item.price > item.avgCost
+                      ? 'highlight-green'
+                      : item.price < item.avgCost
+                      ? 'highlight-red'
+                      : 'highlight-white'
+                  }
+                >
+                <td>{key}</td>
+                <TableCell item={item} attribute="price" />
+                <TableCell item={item} attribute="avgCost" />
+                <TableCell item={item} attribute="quantity" />
+              </tr>
+          ))}
+      </tbody>
+      </table>
+  );
+});
+
+// Memoize the table cell, so no need update whole table when attribute changes
+const TableCell = React.memo(({ item, attribute }: { item: Record<string, any>, attribute: string }) => {
+  const value = useMemo(() => item[attribute], [item, attribute]);
+
+  return <td>{value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>;
+});
+
+export default ItemList;
